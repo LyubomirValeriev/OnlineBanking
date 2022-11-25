bed5cf5e91e6dd5b81cc51d081f7f703c4f7df86
Lyubomir <luybomir2001@abv.bg>
2022-11-25 18:13:56 +0200

    Add User.cs

---
+++ OnlineBanking/Models/TransactionHistoryModels.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ProjectMVC.Models;
+
+namespace OnlineBanking.Models
+{
+    public class TransactionHistoryModels
+    {
+        public List<TransactionListModels> TransactionListModels { get; set; }
+    }
+}
---
+++ OnlineBanking/Models/User.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.RegularExpressions;
+
+namespace OnlineBanking.Models
+{
+    public class User
+    {
+        private const string PHONE_NUMBER_FORMAT = "{0:(61)- ### ### ###}";
+
+        //Unique ID Required
+        [Required, Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int UserID { get; set; }
+
+        [Required, MaxLength(50), RegularExpression(@"^[A-Za-z\s]+$", ErrorMessage = "Only letters of the alphabet")]
+        [Display(Name = "Customer Name")]
+        public string UserName { get; set; }
+
+
+        [MaxLength(50), RegularExpression(@"^\b[\dA-Za-z\s\-\\]+\b$", ErrorMessage = "Only letters and numbers")]
+        public string Address { get; set; }
+
+        [MaxLength(40), RegularExpression(@"^\b[A-Za-z\s]+$", ErrorMessage = "Please only use names.")]
+        public string City { get; set; }
+
+        //Must be a 4 digit number
+        [RegularExpression(@"\b\d{4}\b", ErrorMessage = "Four digits only")]
+        public string PostCode { get; set; }
+
+        ////Must be in the format (61)- xxxx xxxx
+        //[Required, StringLength(18, MinimumLength = 10, ErrorMessage = "Phone Number must be inputted with 10 digits")]
+        //public string Phone { get; set; }
+
+        private string _phone;
+
+        [RegularExpression(@"^0{1}[\d]{9}$", ErrorMessage = "Must be an Australian Mobile number")]
+        public string Phone
+        {
+            get
+            {
+                return $"0{_phone = Regex.Replace(_phone, @"(\(61\)|\-|\s)", "")}";
+            }
+            set
+            {
+                _phone = string.Format(PHONE_NUMBER_FORMAT, Convert.ToInt32(value));
+            }
+        }
+
+        [NotMapped]
+        public string DisplayPhoneNumber => _phone;
+
+    }
+}
