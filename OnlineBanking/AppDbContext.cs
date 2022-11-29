79b684a47eb986d85d141616f7959a6910bd5d18
Lyubomir <luybomir2001@abv.bg>
2022-11-29 17:24:15 +0200

    Db

---
+++ OnlineBanking/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineBanking.Models;
+
+namespace OnlineBanking
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> users { get; set; }
+    }
+}
--- OnlineBanking/Program.cs
+++ OnlineBanking/Program.cs
@@ -1,11 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using OnlineBanking;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 
-var app = builder.Build();
 
+var connectionString = builder.Configuration.GetConnectionString("DefautConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseNpgsql(connectionString));
+
+
+var app = builder.Build();
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
